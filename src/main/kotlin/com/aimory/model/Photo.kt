7bfc4a216c56351ba6a137b77bca38eff7604fbf
overguy723 <156625964+overguy723@users.noreply.github.com>
2025-01-24 22:33:20 +0900

    REFACTOR(#3): Photo 엔티티 주 생성자 생략 및 protected set 설정

--- src/main/kotlin/com/aimory/model/Photo.kt
+++ src/main/kotlin/com/aimory/model/Photo.kt
@@ -5,16 +5,20 @@ import jakarta.persistence.*
 @Entity
 @Table(name = "photo")
 class Photo(
+    imageUrl: String,
+    child: Child
+) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
+    val id: Long = 0
 
     @Column(name = "image_url", nullable = false)
-    var imageUrl: String,
+    var imageUrl: String = imageUrl
+        protected set
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "child_id", nullable = false)
-    var child: Child
-) {
-    constructor(imageUrl: String, child: Child) : this(0, imageUrl, child)
+    var child: Child = child
+        protected set
 }
+
