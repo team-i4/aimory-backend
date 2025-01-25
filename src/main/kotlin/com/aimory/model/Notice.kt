4416db2af2900109e4a61b6c469f34906f3c08c0
moonxxpower <su551732@gmail.com>
2025-01-25 11:04:12 +0900

    FEAT(#2): Notice, Notice Image Entity 선언

--- src/main/kotlin/com/aimory/model/Notice.kt
+++ src/main/kotlin/com/aimory/model/Notice.kt
@@ -1,8 +1,47 @@
 package com.aimory.model
 
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
+import java.time.LocalDate
 
 @Entity
 @Table(name = "notice")
-class Notice()
+class Notice(
+    title: String,
+    content: String,
+    date: LocalDate,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+
+    @Column(name = "title", nullable = false)
+    var title: String = title
+        protected set
+
+    @Column(name = "content", nullable = false)
+    var content: String = content
+        protected set
+
+    @Column(name = "date", nullable = false)
+    var date: LocalDate = date
+        protected set
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: LocalDate = LocalDate.now()
+        protected set
+
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDate = createdAt
+        protected set
+
+    @OneToMany(mappedBy = "notice", orphanRemoval = true, cascade = [CascadeType.ALL])
+    var noticeImages: MutableList<NoticeImage> = mutableListOf()
+        protected set
+}
