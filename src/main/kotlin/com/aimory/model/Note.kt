0bee68b915126fc5ebfc90b869335ee7a5e0759b
moonxxpower <su551732@gmail.com>
2025-01-25 21:57:02 +0900

    FEAT(#12): Note, Note Image Entity 선언

--- src/main/kotlin/com/aimory/model/Note.kt
+++ src/main/kotlin/com/aimory/model/Note.kt
@@ -1,9 +1,36 @@
 package com.aimory.model
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.Table
+import java.time.LocalDate
 
 @Entity
 @Table(name = "note")
 class Note(
-)
+    content: String,
+    date: LocalDate,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+
+    @Column(name = "content", nullable = false)
+    var content: String = content
+        protected set
+
+    @Column(name = "date", nullable = false)
+    var date: LocalDate = date
+        protected set
+
+    @Column(name = "created_at", nullable = false)
+    var createdAt: LocalDate = LocalDate.now()
+        protected set
+
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDate = createdAt
+        protected set
+}
--- src/main/kotlin/com/aimory/model/NoteImage.kt
+++ src/main/kotlin/com/aimory/model/NoteImage.kt
@@ -1,9 +1,31 @@
 package com.aimory.model
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 
 @Entity
 @Table(name = "note_image")
 class NoteImage(
-)
+    imageUrl: String,
+    note: Note,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+
+    @Column(name = "image_url", nullable = false)
+    var imageUrl: String = imageUrl
+        protected set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "note_id", nullable = false)
+    var note: Note = note
+        protected set
+}
