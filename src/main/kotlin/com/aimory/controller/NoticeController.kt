f475bdca19e16bc53481512a5a2019567f73759c
moonxxpower <su551732@gmail.com>
2025-01-25 11:08:15 +0900

    FEAT(#2): 공지사항 생성 API 추가

--- src/main/kotlin/com/aimory/controller/NoticeController.kt
+++ src/main/kotlin/com/aimory/controller/NoticeController.kt
@@ -1,9 +1,27 @@
 package com.aimory.controller
 
+import com.aimory.controller.dto.NoticeCreateRequest
+import com.aimory.controller.dto.NoticeResponse
+import com.aimory.controller.dto.toRequestDto
+import com.aimory.controller.dto.toResponse
 import com.aimory.service.NoticeService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class NoticeController(
     private val noticeService: NoticeService,
-)
+) {
+    /**
+     * 공지사항 생성
+     */
+    @PostMapping("/notices")
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createNotice(@RequestBody noticeCreateRequest: NoticeCreateRequest): NoticeResponse {
+        val noticeDto = noticeService.createNotice(noticeCreateRequest.toRequestDto())
+        return noticeDto.toResponse()
+    }
+}
--- src/main/kotlin/com/aimory/service/NoticeService.kt
+++ src/main/kotlin/com/aimory/service/NoticeService.kt
@@ -1,9 +1,21 @@
 package com.aimory.service
 
 import com.aimory.repository.NoticeRepository
+import com.aimory.service.dto.NoticeCreateRequestDto
+import com.aimory.service.dto.NoticeResponseDto
+import com.aimory.service.dto.toEntity
+import com.aimory.service.dto.toResponseDto
 import org.springframework.stereotype.Service
 
 @Service
 class NoticeService(
     private val noticeRepository: NoticeRepository,
-)
+) {
+    /**
+     * 공지사항 생성
+     */
+    fun createNotice(noticeCreateRequestDto: NoticeCreateRequestDto): NoticeResponseDto {
+        val notice = noticeRepository.save(noticeCreateRequestDto.toEntity())
+        return notice.toResponseDto()
+    }
+}
