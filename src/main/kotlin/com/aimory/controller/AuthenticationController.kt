555e66a358f7fc9ae31c1d6fe5575701ce2e3e6d
training0913 <0193market@gmail.com>
2025-01-29 01:16:35 +0900

    FEAT(#7): 로그인 API 추가

---
+++ src/main/kotlin/com/aimory/controller/AuthenticationController.kt
@@ -0,0 +1,30 @@
+package com.aimory.controller
+
+import com.aimory.controller.dto.AuthenticationRequest
+import com.aimory.controller.dto.AuthenticationResponse
+import com.aimory.exception.UnauthorizedException
+import com.aimory.security.JwtAuthenticationToken
+import com.aimory.service.dto.AuthenticationDto
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class AuthenticationController(private val authenticationManager: AuthenticationManager) {
+
+    @PostMapping("/login")
+    fun authentication(@RequestBody request: AuthenticationRequest): AuthenticationResponse {
+        try {
+            val authToken: JwtAuthenticationToken = JwtAuthenticationToken(request.email, request.password)
+            val authentication: Authentication = authenticationManager.authenticate(authToken)
+            SecurityContextHolder.getContext().authentication = authentication
+            return AuthenticationResponse(authentication.details as AuthenticationDto)
+        } catch (e: AuthenticationException) {
+            throw UnauthorizedException(e.message)
+        }
+    }
+}
