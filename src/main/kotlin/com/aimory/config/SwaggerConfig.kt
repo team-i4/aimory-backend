11a669ebf89da728fd9de438f35600c295bec8ab
moonxxpower <su551732@gmail.com>
2025-02-02 03:15:28 +0900

    FEAT(#22): 'application/octet-stream' 요청 변환기 추가

--- src/main/kotlin/com/aimory/config/SwaggerConfig.kt
+++ src/main/kotlin/com/aimory/config/SwaggerConfig.kt
@@ -1,12 +1,17 @@
 package com.aimory.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Info
-import io.swagger.v3.oas.models.Components
-import io.swagger.v3.oas.models.security.SecurityScheme
 import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter
+import org.springframework.lang.Nullable
+import java.lang.reflect.Type
 
 @Configuration
 class SwaggerConfig {
@@ -17,7 +22,8 @@ class SwaggerConfig {
             .info(configurationInfo())
             .components(
                 Components()
-                    .addSecuritySchemes("api_key",
+                    .addSecuritySchemes(
+                        "api_key",
                         SecurityScheme()
                             .type(SecurityScheme.Type.APIKEY)
                             .`in`(SecurityScheme.In.HEADER)
@@ -32,5 +38,32 @@ class SwaggerConfig {
             .title("아이모리 API 명세서")
             .description("아이포 팀 프로젝트✌️")
             .version("1.0.0")
+    }
+
+    /**
+     * 'application/octet-stream' 요청 변환기
+     */
+    @Bean
+    fun multipartJackson2HttpMessageConverter(objectMapper: ObjectMapper): MultipartJackson2HttpMessageConverter {
+        return MultipartJackson2HttpMessageConverter(objectMapper)
+    }
+
+    class MultipartJackson2HttpMessageConverter(
+        objectMapper: ObjectMapper,
+    ) : AbstractJackson2HttpMessageConverter(objectMapper, MediaType.APPLICATION_OCTET_STREAM) {
+        override fun canWrite(
+            clazz: Class<*>,
+            mediaType: MediaType?,
+        ): Boolean = false
+
+        override fun canWrite(
+            @Nullable type: Type?,
+            clazz: Class<*>,
+            @Nullable mediaType: MediaType?,
+        ): Boolean = false
+
+        override fun canWrite(
+            mediaType: MediaType?,
+        ): Boolean = false
     }
 }
