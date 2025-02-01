daabb53012bff046333c2c99d738cf1de9356970
moonxxpower <su551732@gmail.com>
2025-02-01 22:05:49 +0900

    FEAT(#22): Notice, Center 연관관계 매핑

--- src/main/kotlin/com/aimory/model/Notice.kt
+++ src/main/kotlin/com/aimory/model/Notice.kt
@@ -4,9 +4,12 @@ import com.aimory.service.dto.NoticeRequestDto
 import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
 import java.time.LocalDate
@@ -17,6 +20,7 @@ class Notice(
     title: String,
     content: String,
     date: LocalDate,
+    center: Center,
 ) {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -40,6 +44,11 @@ class Notice(
 
     @Column(name = "updated_at", nullable = false)
     var updatedAt: LocalDate = createdAt
+        protected set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "center_id", nullable = false)
+    var center: Center = center
         protected set
 
     @OneToMany(mappedBy = "notice", orphanRemoval = true, cascade = [CascadeType.ALL])
