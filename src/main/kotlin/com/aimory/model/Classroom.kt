42546c1cb24e01f4c164797a67675857fe57494e
moonxxpower <su551732@gmail.com>
2025-02-02 10:07:48 +0900

    FEAT(#33): Classroom Entity 선언

---
+++ src/main/kotlin/com/aimory/model/Classroom.kt
@@ -0,0 +1,31 @@
+package com.aimory.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "class")
+class Classroom(
+    name: String,
+    teacher: Teacher,
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0
+
+    @Column(name = "name", nullable = false)
+    var name: String = name
+        protected set
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id", nullable = false)
+    var teacher: Teacher = teacher
+        protected set
+}
